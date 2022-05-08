13aa7f4f0d1ff470ee63c4ce50478f566461e9ab
hahyeyoung <fnskfnsl@naver.com>
2022-05-08 23:13:27 +0900

    2022.5.8

--- Baekjoon/기본수학2/소수 찾기.py
+++ Baekjoon/기본수학2/소수 찾기.py
@@ -1,1 +1,14 @@
 N = int(input())
+num = list(map(int, input().split()))
+
+for i in num:
+    sum = 0
+    if i!=1 :
+        check = True
+    for j in range(2, i):
+        if i % j == 0 :
+            check = False
+            break
+        if check :
+            sum += 1
+print(sum)
