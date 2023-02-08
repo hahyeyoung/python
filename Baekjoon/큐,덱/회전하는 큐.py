d6c351120949b186451796e6713fb9e13bc5d19e
hahyeyoung <fnskfnsl@naver.com>
2023-02-08 23:53:14 +0900

    2023.2.8

--- Baekjoon/큐,덱/회전하는 큐.py
+++ Baekjoon/큐,덱/회전하는 큐.py
@@ -2,6 +2,19 @@ from collections import deque
 n, m = map(int,input().split())
 num = list(map(int,input().split()))
 queue =deque([i for i in range(1,n+1)])
-print(queue)
 
 result = 0
+
+for i in num:
+    while True:
+        if i == queue[0]:
+            queue.popleft()
+            break
+        else :
+            if queue.index(i) <= len(queue)//2:
+                queue.rotate(-1)
+                result +=1
+            else :
+                queue.rotate(1)
+                result +=1
+print(result)
